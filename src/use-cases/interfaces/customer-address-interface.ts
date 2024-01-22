dfd9d07af0a48914a336fd62a695acb3fe1f0f7d
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-01-21 23:40:56 -0300

    feat: added id in interfaces

---
+++ src/use-cases/interfaces/customer-address-interface.ts
@@ -0,0 +1,14 @@
+export interface CustomerAddress {
+  id: string
+  name: string
+  phone: string
+  email: string
+  street: string
+  number: string
+  neighborhood: string
+  city: string
+  state: string
+  postal_code: string
+  latitude: number
+  longitude: number
+}
