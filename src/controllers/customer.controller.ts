84d91dfc641f65573bf5e3eb9f11eb5e228d1da5
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-01-21 23:43:43 -0300

    feat: changed routes and controller

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -4,6 +4,7 @@ import { makeCreateCustomerUseCase } from '../use-cases/factories/make-create-cu
 import { makeListCustomerUseCase } from '../use-cases/factories/make-list-customer.use-case'
 import { Request, Response } from 'express'
 import { makeListCustomerAddressUseCase } from '../use-cases/factories/make-list-customer-address-sorted.use-case'
+import { makeCreateAddressCustomerUseCase } from '../use-cases/factories/make-create-customer-address.use-case'
 
 export class CustomerController {
   static async listCustomers(req: Request, res: Response) {
@@ -52,6 +53,74 @@ export class CustomerController {
       const createCustomerUseCase = makeCreateCustomerUseCase()
 
       await createCustomerUseCase.execute({ name, email, phone })
+
+      return res.status(204).json({
+        message: 'created successfully.',
+      })
+    } catch (error) {
+      if (error instanceof CustomerAlreadyExists) {
+        return res.status(409).send({ message: error.message })
+      }
+
+      if (error instanceof ZodError) {
+        return res
+          .status(400)
+          .json({ message: 'Validation error.', issues: error.issues })
+      }
+      console.error(error)
+      throw error
+    }
+  }
+
+  static async createCustomerAddress(req: Request, res: Response) {
+    try {
+      const createCustomerAddressBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        phone: z.string(),
+        street: z.string(),
+        number: z.string(),
+        neighborhood: z.string(),
+        city: z.string(),
+        state: z.string(),
+        postalCode: z.string(),
+        latitude: z.coerce.number().refine((value) => {
+          return Math.abs(value) <= 90
+        }),
+        longitude: z.coerce.number().refine((value) => {
+          return Math.abs(value) <= 180
+        }),
+      })
+
+      const {
+        name,
+        email,
+        phone,
+        street,
+        number,
+        neighborhood,
+        city,
+        state,
+        postalCode,
+        latitude,
+        longitude,
+      } = createCustomerAddressBodySchema.parse(req.body)
+
+      const createCustomerUseCase = makeCreateAddressCustomerUseCase()
+
+      await createCustomerUseCase.execute({
+        name,
+        email,
+        phone,
+        street,
+        number,
+        neighborhood,
+        city,
+        state,
+        postal_code: postalCode,
+        latitude,
+        longitude,
+      })
 
       return res.status(204).json({
         message: 'created successfully.',
--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,15 @@ import { CustomerController } from './controllers/customer.controller'
 
 const routes = express.Router()
 
+/* Customer */
 routes.get('/customers', CustomerController.listCustomers)
+routes.post('/customer', CustomerController.createCustomer)
+
+/* Customer Address */
 routes.get(
   '/customer-addresses',
   CustomerController.listCustomerAddressesSorted,
 )
-routes.post('/customer', CustomerController.createCustomer)
+routes.post('/customer-address', CustomerController.createCustomerAddress)
 
 export default routes
